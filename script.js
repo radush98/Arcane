753bede98f92e911aab8e55f92577f89e93a6527
Valdimir Radush <radush9860@gmail.com>
2022-01-05 17:51:57 +0200

    Arcane based web-site - main page

---
+++ script.js
@@ -0,0 +1,17 @@
+// window.onload = function () {
+//     const mainAnimation = document.querySelector('#main-video');
+//     play();
+
+//     function play() {
+//         mainAnimation.currentTime = 0;
+//         const videoPromise = mainAnimation.play();
+//         if (videoPromise !== undefined) {
+//             videoPromise.then(() => {
+//                 mainAnimation.play()
+//                 // setTimeout(() => mainAnimation.pause(), 12200)
+//             }).catch(error => {
+//                 console.log(error);
+//             })
+//         }
+//     }
+// }
